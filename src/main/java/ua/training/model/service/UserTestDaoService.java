da961cab6bb1dd1f2b1101420dcff642ab8a1eef
Olena Hedz <Lenka-949@mail.ru>
2018-05-17 11:13:31 +0300

    add findByUser

--- src/main/java/ua/training/model/service/UserTestDaoService.java
+++ src/main/java/ua/training/model/service/UserTestDaoService.java
@@ -16,6 +16,8 @@ public class UserTestDaoService {
 
     public static List<UserTest> findAll() { return dao.findAll(); }
 
+    public static List<UserTest> findByUser(long iduser) { return dao.findByUser(iduser); }
+
     public static void update(UserTest userTest){ dao.update(userTest); }
 
     public static void delete(long id) { dao.delete(id); }
