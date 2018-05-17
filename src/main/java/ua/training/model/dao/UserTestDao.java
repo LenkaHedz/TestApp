520bc56470ac309f6eda7049f8d09e96e631c00d
Olena Hedz <Lenka-949@mail.ru>
2018-05-17 11:12:15 +0300

    add findByUser

--- src/main/java/ua/training/model/dao/UserTestDao.java
+++ src/main/java/ua/training/model/dao/UserTestDao.java
@@ -2,5 +2,8 @@ package ua.training.model.dao;
 
 import ua.training.model.entity.UserTest;
 
+import java.util.List;
+
 public interface UserTestDao extends GenericDao<UserTest> {
+    List<UserTest> findByUser(long iduser);
 }
