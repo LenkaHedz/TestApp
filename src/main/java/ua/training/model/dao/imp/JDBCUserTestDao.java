1d0c68eb03b373241a5a667db91c1a92fb349a73
Olena Hedz <Lenka-949@mail.ru>
2018-05-17 11:12:53 +0300

    add findByUser

--- src/main/java/ua/training/model/dao/imp/JDBCUserTestDao.java
+++ src/main/java/ua/training/model/dao/imp/JDBCUserTestDao.java
@@ -74,6 +74,21 @@ public class JDBCUserTestDao implements UserTestDao {
     }
 
     @Override
+    public List<UserTest> findByUser(long iduser) {
+        List<UserTest> resultList = new ArrayList<>();
+        try (PreparedStatement ps = connection.prepareStatement(Queries.USER_TEST_FIND_BY_USER_ID)){
+            ps.setLong(1, iduser);
+            ResultSet rs = ps.executeQuery();
+            while ( rs.next() ){
+                resultList.add(extractFromResultSet(rs));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return resultList;
+    }
+
+    @Override
     public void update(UserTest userTest){
         try (PreparedStatement ps = connection.prepareStatement(Queries.USER_TEST_UPDATE)){
         ps.setLong(1 , userTest.getUser().getId());
